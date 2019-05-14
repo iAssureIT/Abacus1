492a21113f3a3d2de922d5deea4aca850c81f691
anaghakomawar <madhavir1982@gmail.com>
2019-05-14 14:35:13 +0530

    Anagha worked on signup and signin

---
+++ reactapiserver/api/controllers/user.js
@@ -0,0 +1,114 @@
+const mongoose	= require("mongoose");
+const bcrypt	= require("bcrypt");
+const jwt		= require("jsonwebtoken");
+
+const User = require('../models/users');
+
+exports.user_signup = (req,res,next)=>{
+	User.find({email: req.body.email})
+		.exec()
+		.then(user =>{
+			if(user.length >= 1){
+				console.log('user ',user);
+				return res.status(409).json({
+					message: ' Email Id already exits.'
+				});
+			}else{
+				console.log('email ',req.body.email);
+				bcrypt.hash(req.body.password,10,(err,hash)=>{
+					if(err){
+						return res.status(500).json({
+							error:err
+						});
+					}else{
+
+						const user = new User({
+			              _id: new mongoose.Types.ObjectId(),
+			              email: req.body.email,
+			              password: hash
+			            });	
+						user.save()
+							.then(result =>{
+								console.log('user ',result);
+								res.status(201).json({
+									message: 'User created'
+								})
+							})
+							.catch(err =>{
+								console.log(err);
+								res.status(500).json({
+									error: err
+								});
+							});
+					}			
+				});
+			}
+		})
+		.catch(err =>{
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
+	
+};
+
+exports.user_login = (req,res,next)=>{
+	User.find({email: req.body.email})
+		.exec()
+		.then(user => {
+			if(user.length < 1){
+				return res.status(401).json({
+					message: 'Auth failed'
+				});
+			}
+			bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+				if(err){
+					return res.status(401).json({
+						message: 'Auth failed'
+					});		
+				}
+				if(result){
+					const token = jwt.sign({
+						email 	: user[0].email,
+						userId	: user[0]._id,
+					},process.env.JWT_KEY,
+					{
+						expiresIn: "1h"
+					}
+					);
+					return res.status(200).json({
+						message: 'Auth successful',
+						token: token
+					});	
+				}
+				return res.status(401).json({
+					message: 'Auth failed'
+				});
+			})
+		})
+		.catch(err =>{
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
+};
+
+exports.user_delete = (req,res,next)=>{
+	console.log('user id ',req.params.userId);
+	User.remove({_id:req.params.userId})
+		.exec()
+		.then(result =>{
+			console.log('user delete ',result);
+			res.status(200).json({
+				message: 'User deleted'
+			});
+		})
+		.catch(err =>{
+			console.log('user error ',err);
+			res.status(500).json({
+				error: err
+			});
+		});
+}
