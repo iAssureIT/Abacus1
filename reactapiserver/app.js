4a5cb54cd69384778f6f5881155df029e1c1e77a
anaghakomawar <madhavir1982@gmail.com>
2019-07-04 17:34:06 +0530

    Anagha worked on CORS

--- reactapiserver/app.js
+++ reactapiserver/app.js
@@ -42,10 +42,11 @@ app.use(bodyParser.json());// it show json data in good manner
 app.use((req, res, next) =>{
 	res.header("Access-Control-Allow-origin", "*"); // use API from anywhere insted of * we use domain
 	res.header("Access-Control-Allow-Headers" , "Origin, X-Requested-With, Content-Type, Accept");
-	// req.header('Access-Control-Allow-Methods : PATCH, PUT, POST, DELETE, GET, OPTIONS');
-	// res.writeHead(200, headers);
+	req.header('Access-Control-Allow-Methods : PATCH, PUT, POST, DELETE, GET, OPTIONS');
+	res.writeHead(200, headers);
 	if (req.method === 'OPTIONS') {
 		console.log('options');
+		res.header("Access-Control-Allow-Headers" , "Origin, X-Requested-With, Content-Type, Accept");
 		req.header('Access-Control-Allow-Methods" , "PATCH, PUT, POST, DELETE, GET, OPTIONS');
 		return res.status(200).json({});
 	}
