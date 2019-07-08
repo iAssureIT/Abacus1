c0eac933a3555f7b505a8055263972beabf33b52
anaghakomawar <madhavir1982@gmail.com>
2019-07-08 11:34:56 +0530

    Anagha worked on CORS

--- reactapiserver/app.js
+++ reactapiserver/app.js
@@ -44,10 +44,10 @@ app.use((req, res, next) =>{
 	res.header("Access-Control-Allow-origin", "*"); // use API from anywhere insted of * we use domain
 	res.header("Access-Control-Allow-Headers" , "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	// req.header("Access-Control-Allow-Methods" , "PATCH, PUT, POST, DELETE, GET, OPTIONS");
-	res.writeHead(200, headers);
+	// res.writeHead(200, headers);
 	if (req.method === 'OPTIONS') {
 		console.log('options');
-		res.header("Access-Control-Allow-Headers" , "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		// res.header("Access-Control-Allow-Headers" , "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		req.header("Access-Control-Allow-Methods" , "PATCH, PUT, POST, DELETE, GET, OPTIONS");
 		return res.status(200).json({});
 	}
