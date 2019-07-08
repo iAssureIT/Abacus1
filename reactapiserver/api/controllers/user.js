a4deea19c87c30b38d96052be47e2253b14a5862
anaghakomawar <madhavir1982@gmail.com>
2019-07-08 15:00:30 +0530

    Anagha login

--- reactapiserver/api/controllers/user.js
+++ reactapiserver/api/controllers/user.js
@@ -171,6 +171,7 @@ exports.user_login = (req,res,next)=>{
 								expiresIn: "1h"
 							}
 							);
+							res.header("Access-Control-Allow-Origin","*");
 							return res.status(200).json({
 								message				: 'Auth successful',
 								token				: token,
