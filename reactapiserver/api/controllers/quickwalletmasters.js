ac75519b67142b0a6afe8c5dc026f6fba462203f
anaghakomawar <madhavir1982@gmail.com>
2019-07-08 10:55:26 +0530

    Anagha changed the CORS

--- reactapiserver/api/controllers/quickwalletmasters.js
+++ reactapiserver/api/controllers/quickwalletmasters.js
@@ -2,6 +2,8 @@ const mongoose	= require("mongoose");
 
 const QuickWalletMasters        = require('../models/quickwalletmasters');
 const StudentMaster             = require('../models/studentmasters');
+const ExamMaster                = require('../models/exammasters');
+const CompetitionRegisterOrder  = require('../models/competitionregisterorders');
 
 exports.fetch_details = (req,res,next)=>{
     QuickWalletMasters.find()
@@ -66,5 +68,97 @@ exports.makepayment = (req,res,next) =>{
 }
 
 exports.paymentGatewayforCompetition = (req,res,next) => {
-    StudentMaster
+    StudentMaster   .findOne({studentId:req.params.studentId})
+                    .exec()
+                    .then(studentMasterData=>{
+                        if(studentMasterData){
+                            ExamMaster.findOne({_id:req.params.competitionId})
+                                        .exec()
+                                        .then(ExamMasterData=>{
+                                            if(ExamMasterData){
+                                                const competitionRegisterOrder = new CompetitionRegisterOrder({
+                                                    'competitionId'	            : req.params.competitionId,
+                                                    'competitionName'           : ExamMasterData.competitionName,
+                                                    'studentFullName'           : studentMasterData.studentFullName,
+                                                    'studentId' 	            : req.params.studentId,
+                                                    'category'                  : studentMasterData.category,
+                                                    'subCategory'               : studentMasterData.subCategory,
+                                                    'franchiseId'               : studentMasterData.franchiseId,
+                                                    'franchiseName'             : studentMasterData.franchiseName,
+                                                    "competitionFees"           : req.params.compFees,
+                                                    "competitionOriginalFees"   : req.params.compFees,
+                                                    "franchiseShare"            : ExamMasterData.franchiseShare,
+                                                    "maatsShare"                : ExamMasterData.maatsShare,
+                                                    'status'    	            : 'UnPaid',
+                                                });
+                                                competitionRegisterOrder.save()
+                                                                        .exec()
+                                                                        .then(data=>{
+                                                                            QuickWalletMaster.findOne({})
+                                                                                             .exe()
+                                                                                             .then(QWCredential=>{
+                                                                                                 if(QWCredential){
+                                                                                                    if(QWCredential.environment=="production"){
+                                                                                                        var API = QWCredential.prodAPI;
+                                                                                                        var partnerid = QWCredential.prodKey;
+                                                                                                        var secret    = QWCredential.prodSecret;
+                                                                                                    }else{
+                                                                                                        var API       = QWCredential.sandboxAPI;
+                                                                                                        var partnerid = QWCredential.sandboxKey;
+                                                                                                        var secret    = QWCredential.sandboxSecret;
+                                                                                                    }
+                                                                                                    var quickWalletInput = {
+                                                                                                               "partnerid" : partnerid,
+                                                                                                               "mobile"   :   mobileNumber,
+                                                                                                               "secret"   :   secret,
+                                                                                                               "amount"   :    competitionFees,
+                                                                                                               "redirecturl" : Meteor.absoluteUrl()+'payment-response/'+userId+'/'+comp_id,             
+                                                                                                    };
+                                                                                                    if(quickWalletInput){
+                                                                                                        var result = HTTP.call("POST", API+"/api/partner/"+quickWalletInput.partnerid+"/requestpayment",
+                                                                                                                            {params: quickWalletInput});
+                                                                                                        if(result.data.status == 'success'){
+                                                                                                            var paymentUrl = result.data.data.url;
+                                                                                                            res.status(200).json(paymentUrl);
+                                                                                                        }else{
+                                                                                                            res.status(200).json(false);
+                                                                                                        }
+                                                                                                    }
+                                                                                                 }else{
+                                                                                                     res.status(409).json({message:"Quick wallet details not found."})
+                                                                                                 }
+                                                                                             })
+                                                                                             .catch(err =>{
+                                                                                                console.log(err);
+                                                                                                res.status(500).json({
+                                                                                                    error: err
+                                                                                                });
+                                                                                            });                      
+                                                                        })
+                                                                        .catch(err =>{
+                                                                            console.log(err);
+                                                                            res.status(500).json({
+                                                                                error: err
+                                                                            });
+                                                                        });
+                                            }else{
+                                                res.status(409).json({message:"Exam not found"});
+                                            }
+                                        })
+                                        .catch(err =>{
+                                            console.log(err);
+                                            res.status(500).json({
+                                                error: err
+                                            });
+                                        });
+                        }else{
+                            res.status(409).json({message:"Student Not found"})
+                        }
+                    })
+                    .catch(err =>{
+                        console.log(err);
+                        res.status(500).json({
+                            error: err
+                        });
+                    });
 }
