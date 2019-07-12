97983a892e217b02ea271bdc6646aaf4f08d05b3
anaghakomawar <madhavir1982@gmail.com>
2019-07-12 18:40:45 +0530

    Anagha working on practise exam

--- reactapiserver/api/routes/mypracticeexammasters.js
+++ reactapiserver/api/routes/mypracticeexammasters.js
@@ -7,7 +7,7 @@ router.get('/:studentId', MyPracticeExamMastersController.fetch_practice_student
 router.get('/:examPaperId/:studentId', MyPracticeExamMastersController.fetch_exampaper_student);
 router.get('/practiceExam/:practiceExamId/:studentId',MyPracticeExamMastersController.fetch_practice_exam_student);
 router.get('/practiceExam/:practiceExamId',MyPracticeExamMastersController.fetch_practice);
-
+router.post('/',MyPracticeExamMastersController.update_exam_ans);
 
 
 module.exports = router;
