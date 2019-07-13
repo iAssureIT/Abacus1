f92a57201ab42aab069f1cee26dcbd0d07e0537b
anaghakomawar <madhavir1982@gmail.com>
2019-07-13 19:49:59 +0530

    Anagha change port'

--- reactapiserver/server.js
+++ reactapiserver/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app'); // app file include
- const port = process.env.PORT || 3006;
+ const port = process.env.PORT || 3042;
 
  const server = http.createServer(app);
 
