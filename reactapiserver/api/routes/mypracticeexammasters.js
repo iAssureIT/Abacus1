ef13421b8a3b3bc843c8adb21798aaf419bb8bb7
anaghakomawar <madhavir1982@gmail.com>
2019-07-14 13:44:06 +0530

    Anagha'

--- reactapiserver/api/routes/mypracticeexammasters.js
+++ reactapiserver/api/routes/mypracticeexammasters.js
@@ -4,10 +4,10 @@ const MyPracticeExamMastersController         = require('../controllers/mypracti
 
 router.get('/getresult/:examId',MyPracticeExamMastersController.ExamMarksUpdate);
 router.get('/incompleteexam/:studentId',MyPracticeExamMastersController.fetch_incomplete_student);
+router.get('/practiceExam/:practiceExamId',MyPracticeExamMastersController.fetch_practice);
+router.get('/practiceExam/:practiceExamId/:studentId',MyPracticeExamMastersController.fetch_practice_exam_student);
 router.get('/:studentId', MyPracticeExamMastersController.fetch_practice_student);
 router.get('/:examPaperId/:studentId', MyPracticeExamMastersController.fetch_exampaper_student);
-router.get('/practiceExam/:practiceExamId/:studentId',MyPracticeExamMastersController.fetch_practice_exam_student);
-router.get('/practiceExam/:practiceExamId',MyPracticeExamMastersController.fetch_practice);
 router.post('/',MyPracticeExamMastersController.update_exam_ans);
 
 
