e15bbe8d03197cbad945052180adc348272dcb96
anaghakomawar <madhavir1982@gmail.com>
2019-07-16 20:49:52 +0530

    Anagha

--- reactapiserver/api/routes/startexamcategorywise.js
+++ reactapiserver/api/routes/startexamcategorywise.js
@@ -75,7 +75,7 @@ router.post('/:compId/:studentID', (req,res,next)=>{
                                                                                         if(tempQueArray.length == questionArray1.length){
                                                                                             const myexammaster = new MyExamMaster({
                                                                                                                     "_id"                   : new mongoose.Types.ObjectId(),
-                                                                                                                    "StudentId"             : req.params.studentId,
+                                                                                                                    "StudentId"             : req.params.studentID,
                                                                                                                     "companyId"             : studentData.companyId,
                                                                                                                     "franchise_id"          : studentData.franchiseId,
                                                                                                                     "firstName"             : studentData.studentFirstName,
