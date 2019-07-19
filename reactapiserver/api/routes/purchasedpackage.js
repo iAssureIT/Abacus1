807172643e6543db4f8c82bf814f86f43660126f
anaghakomawar <madhavir1982@gmail.com>
2019-07-19 15:45:19 +0530

    Anagha

--- reactapiserver/api/routes/purchasedpackage.js
+++ reactapiserver/api/routes/purchasedpackage.js
@@ -161,8 +161,9 @@ router.post('/updatequespaper', (req,res,next) =>{
                                 console.log('studentId ',req.body.studentId);
                                 console.log('pckgIndex ',req.body.pckgIndex);
                                 console.log('orderId ',req.body.orderId);
-                                var search = {"questionPaper_id":practiceExamId.examPaperId,"buyerId":req.body.studentId,"packageId" : req.body.pckgIndex ,"order_id":req.body.orderId}
-                                PackageQuestionPaperMaster.findOne()
+                                var selector = {"questionPaper_id":practiceExamId.examPaperId,"buyerId":req.body.studentId,"packageId" : req.body.pckgIndex ,"order_id":req.body.orderId}
+                                console.log('selector = ',selector );
+                                PackageQuestionPaperMaster.findOne(selector)
                                 .exec()
                                 .then(questionPaperDetails=>{
                                     console.log('out questionPaperDetails ',questionPaperDetails);
