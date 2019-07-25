8339c268dd4fc9ee59195abbee9418b08563f8ee
angelasmi <asmi3688@gmail.com>
2019-07-25 12:30:07 +0530

    Asmita added code

--- reactapiserver/api/routes/competitionregisterorder.js
+++ reactapiserver/api/routes/competitionregisterorder.js
@@ -2,8 +2,8 @@ const express 	= require("express");
 const router 	= express.Router();
 const CompetitionRegisterOrderController         = require('../controllers/competitionregisterorders');
 
-router.get('/mainexam/:studentId',CompetitionRegisterOrderController.fetch_mycompetitionorder_examStatus);
 router.post('/updateOrder/:studentId/:competitionId/:status/:id/:billNumbers',CompetitionRegisterOrderController.update_mycompetitionorderreceipt);
+router.get('/mainexam/:studentId',CompetitionRegisterOrderController.fetch_mycompetitionorder_examStatus);
 // router.post('/updateOrder/:studentId/:competitionId/:status/:id/:billNumbers',CompetitionRegisterOrderController.update_mycompetitionorderreceipt);
 router.get('/:studentId',CompetitionRegisterOrderController.fetch_mycompetitionorder);
 router.get('/:studentId/:competitionId',CompetitionRegisterOrderController.fetch_mycompetitionorderreceipt);
